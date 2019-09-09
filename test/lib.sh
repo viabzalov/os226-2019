6931dedeb13a8e63accc9c319ca7de287d201c16
Anton Kozlov <drakon.mega@gmail.com>
2019-09-09 23:16:13 +0300

    Improve test lib

--- test/lib.sh
+++ test/lib.sh
@@ -1,7 +1,21 @@
 
+set -o pipefail
+
 cmpout() {
 	local base=$1
 	shift 1
 
-	"$@" < $base.in | cmp $base.out
+	"$@" < $base.in | diff -u $base.out -
+}
+
+testouts() {
+	local base=$1
+	shift 1
+
+	local ret=0
+	for i in $base/*.in; do
+		cmpout ${i%%.in} "$@" || ret=1
+	done
+
+	return $ret
 }
