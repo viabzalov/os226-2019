5a3f66a9a830f66edd2ff1b74c855ba3536cc732
Vadim Abzalov <st067798@student.spbu.ru>
2019-09-13 08:27:22 +0300

    Update init.c with Switch

--- syscall/init.c
+++ syscall/init.c
@@ -14,24 +14,48 @@
 void sig_func(int sig, siginfo_t *info, void *ctx) {
     ucontext_t *uc = (ucontext_t *) ctx;
     static int atmpt = 0;
-    if (0x338b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
-        uc->uc_mcontext.gregs[REG_RSI] = 100000 + (++atmpt);
-        uc->uc_mcontext.gregs[REG_RIP] += 2;
-    } else if (0x738b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
-        int i = ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18;
-        uc->uc_mcontext.gregs[REG_RSI] = 100000 + 1000 * i + (++atmpt);
-        uc->uc_mcontext.gregs[REG_RIP] += 3;
-    } else if (0x758b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
-        int i = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + 4;
-        uc->uc_mcontext.gregs[REG_RSI] = 100000 + 1000 * i + (++atmpt);
-        uc->uc_mcontext.gregs[REG_RIP] += 3;
-    } else if (0x138b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
-        uc->uc_mcontext.gregs[REG_RDX] = 100000 + (++atmpt);
-        uc->uc_mcontext.gregs[REG_RIP] += 2;
-    } else if (0x558b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
-        int i = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + 4;
-        uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 * i + (++atmpt);
-        uc->uc_mcontext.gregs[REG_RIP] += 3;
+    switch (*(uint16_t *) uc->uc_mcontext.gregs[REG_RIP])
+    {
+        case 0x338b:
+        {
+            uc->uc_mcontext.gregs[REG_RSI] = 100000 + (++atmpt);
+            uc->uc_mcontext.gregs[REG_RIP] += 2;
+        }
+            break;
+            
+        case 0x738b:
+        {
+            int i = ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18;
+            uc->uc_mcontext.gregs[REG_RSI] = 100000 + 1000 * i + (++atmpt);
+            uc->uc_mcontext.gregs[REG_RIP] += 3;
+        }
+            break;
+        
+        case 0x758b:
+        {
+            int i = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + 4;
+            uc->uc_mcontext.gregs[REG_RSI] = 100000 + 1000 * i + (++atmpt);
+            uc->uc_mcontext.gregs[REG_RIP] += 3;
+        }
+            break;
+
+        case 0x138b:
+        {
+            uc->uc_mcontext.gregs[REG_RDX] = 100000 + (++atmpt);
+            uc->uc_mcontext.gregs[REG_RIP] += 2;
+        }
+            break;
+        
+        case 0x558b:
+        {
+            int i = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + 4;
+            uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 * i + (++atmpt);
+            uc->uc_mcontext.gregs[REG_RIP] += 3;
+        }
+            break;
+            
+        default:
+            break;
     } 
 }
 
